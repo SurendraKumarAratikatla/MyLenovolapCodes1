ec043ec78c504ee825ce018c6607339208cdb775
SurendraAratikatla <suendra.aratikatla1608@gmail.com>
2021-06-28 01:23:34 +0530

    MintMesh - Surendra - Program

--- MintMesh_Assignment/Problem2_Mongodb/mintmesh/corona/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import CoronaModel
-
-admin.site.register(CoronaModel)
--- MintMesh_Assignment/Problem2_Mongodb/mintmesh/corona/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CoronaModel
-
-class CORONAListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoronaModel
-        fields = ('data')
